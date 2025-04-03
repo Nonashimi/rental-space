1eabd2c2e2e410c2e683d155d2754b0efb224423
Olzhas Yergali <olzhasergali56@gmail.com>
2025-04-03 13:29:42 +0500

    change the hover problem of markers

--- src/shared/components/apartment-map.tsx
+++ src/shared/components/apartment-map.tsx
@@ -32,7 +32,7 @@ export default function ApartmentMap() {
       const fav = favorites.inFavList(id);
       return L.divIcon({
         className: "custom-marker",
-        html: `<div class="marker-container flex gap-1 hover:scale-105 transition-all duration-300 hover:z-10">
+        html: `<div class="marker-container flex gap-1 hover:scale-105 transition-all duration-300">
                  <span class="marker-text">${price}₽</span>
                  ${fav ? `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="red">
                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
@@ -108,7 +108,11 @@ export default function ApartmentMap() {
           const icon = createCustomIcon(card.price, card.id);
           return (
             icon && (
-              <Marker key={card.id} icon={icon} position={[card.coordinates.lat, card.coordinates.lng]}>
+              <Marker 
+                key={card.id} 
+                icon={icon} 
+                position={[card.coordinates.lat, card.coordinates.lng]}
+                >
                 <Popup>
                   <CardPopup inFavList={favorites.inFavList} clickToFav={favorites.clickToFav} cardItem={card} />
                 </Popup>
