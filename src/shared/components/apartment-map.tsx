3ba51e4160df084e32847789c5ea0009fa2f379c
Olzhas Yergali <olzhasergali56@gmail.com>
2025-04-18 20:29:25 +0500

    divide the apartment map to components

--- src/shared/components/apartment-map.tsx
+++ src/shared/components/apartment-map.tsx
@@ -9,6 +9,7 @@ import CardPopup from "./cart-popup";
 import { Cluster, useClustering } from "@/hooks/useClustering";
 import ListOfCardsPopup from "./list-of-cards-popup";
 import { useMapEvent } from "react-leaflet";
+import { CustomMarker } from "./custom-marker";
 
 // Динамический импорт react-leaflet
 const MapContainer = dynamic(() => import("react-leaflet").then((mod) => mod.MapContainer), { ssr: false });
@@ -22,33 +23,11 @@ export default function ApartmentMap() {
   const [mapCenter, setMapCenter] = useState<[number, number]>([0, 0]);
   const [zoom, setZoom] = useState<number>(2);
   const [geoData, setGeoData] = useState<any>(null);
-  const [L, setL] = useState<any>(null);
   const { cardList } = useCardListStore();  // Предполагается, что в store есть метод updateCardCondition
-  const favorites = useFavoritesStore();
   const [minDestination, setMinDestination] = useState<number>(10.5);
   const {ClusterMarkers} = useClustering(cardList, minDestination);
   const [clusters, setCLusters] = useState<Cluster[]>(ClusterMarkers());
-  useEffect(() => {
-    import("leaflet").then((leaflet) => setL(leaflet));
-  }, []);
-
-  const singleMarker = (condition:MarkerCondition, price:number, id: number) => {
-    const fav = favorites.inFavList(id);
-
-    return `<div class="marker-container ${condition} flex gap-1 hover:scale-[106%] transition-all duration-300">
-                 <span class="marker-text">${price}₽</span>
-                 ${fav ? `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="${condition === MarkerCondition.ACTIVE?"white":"red"}">
-                   <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-                 </svg>` : ""}
-               </div>`;
-  };
-
-  const clusterMarker = (condition: MarkerCondition) => {
-    return  ` <div class="${condition} cluster-marker  flex  hover:scale-[106%] transition-all duration-300">
-    </div>`
-    ;
-  }
-
+ 
 
 
   const updateCondition = (id: number, condition: MarkerCondition) => {
@@ -99,44 +78,32 @@ export default function ApartmentMap() {
   }, [minDestination]);
 
 
-  const createCustomIcon = useCallback(
-    (cluster:Cluster) => {
-      if (!L) return null;
-      return L.divIcon({
-        className: "custom-marker",
-        html: cluster.cardItems.length > 1 ? clusterMarker(cluster.condition) : singleMarker(cluster.condition, cluster.cardItems[0].price, cluster.id),
-        iconSize: [50, 30],
-        iconAnchor: [25, 15],
-      });
-    },
-    [L, favorites]
-  );
 
-  const searchLocation = useCallback(async (query: string) => {
-    try {
-      const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${query}`);
-      const data = await response.json();
+  // const searchLocation = useCallback(async (query: string) => {
+  //   try {
+  //     const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${query}`);
+  //     const data = await response.json();
 
-      if (data.length === 0) {
-        console.warn("Местоположение не найдено.");
-        return;
-      }
+  //     if (data.length === 0) {
+  //       console.warn("Местоположение не найдено.");
+  //       return;
+  //     }
 
-      const { lat, lon, boundingbox } = data[0];
-      const latDiff = Math.abs(parseFloat(boundingbox[0]) - parseFloat(boundingbox[1]));
-      const newZoom = latDiff > 20 ? 4 : latDiff > 5 ? 6 : latDiff > 1 ? 8 : 12;
+  //     const { lat, lon, boundingbox } = data[0];
+  //     const latDiff = Math.abs(parseFloat(boundingbox[0]) - parseFloat(boundingbox[1]));
+  //     const newZoom = latDiff > 20 ? 4 : latDiff > 5 ? 6 : latDiff > 1 ? 8 : 12;
 
-      setZoom(newZoom);
-      setMapCenter([parseFloat(lat), parseFloat(lon)]);
+  //     setZoom(newZoom);
+  //     setMapCenter([parseFloat(lat), parseFloat(lon)]);
 
-      const geoResponse = await fetch(`https://nominatim.openstreetmap.org/search?format=geojson&q=${query}&polygon_geojson=1`);
-      const geoJson = await geoResponse.json();
+  //     const geoResponse = await fetch(`https://nominatim.openstreetmap.org/search?format=geojson&q=${query}&polygon_geojson=1`);
+  //     const geoJson = await geoResponse.json();
 
-      setGeoData(geoJson?.features?.length > 0 ? geoJson : null);
-    } catch (error) {
-      console.error("Ошибка при поиске местоположения:", error);
-    }
-  }, []);
+  //     setGeoData(geoJson?.features?.length > 0 ? geoJson : null);
+  //   } catch (error) {
+  //     console.error("Ошибка при поиске местоположения:", error);
+  //   }
+  // }, []);
 
 
   const handleMapCLick = (e:React.MouseEvent<HTMLDivElement>) => {
@@ -182,37 +149,9 @@ export default function ApartmentMap() {
           />
         )}
 
-        {clusters.map((cluster) => {
-          const icon = createCustomIcon(cluster);
-          return (
-            icon && (
-              <Marker
-                key={cluster.cardItems[0].id}
-                icon={icon}
-                position={[cluster.cardItems[0].coordinates.lat, cluster.cardItems[0].coordinates.lng]}
-                eventHandlers={{
-                  click: () => {
-                    clusters.map(cl => {
-                      if (cl.condition === MarkerCondition.ACTIVE) {
-                        updateCondition(cl.id, MarkerCondition.VISITED);
-                      }
-                      return cl;
-                    });
-                    updateCondition(cluster.id, MarkerCondition.ACTIVE);
-                  },
-                }}
-              >
-                <Popup>
-                  {cluster.cardItems.length > 1 ? (
-                    <ListOfCardsPopup cardItems={cluster.cardItems}/>
-                  ) : (
-                  <CardPopup inFavList={favorites.inFavList} clickToFav={favorites.clickToFav} cardItem={cluster.cardItems[0]} />
-                  )}
-                </Popup>
-              </Marker>
-            )
-          );
-        })}
+        {clusters.map((cluster) => 
+         <CustomMarker key={cluster.id} cluster={cluster} clusters={clusters} updateCondition={updateCondition}/>
+        )}
       </MapContainer>
     </div>
   );
---
+++ src/shared/components/custom-marker.tsx
@@ -0,0 +1,88 @@
+"use client"
+import { Cluster } from '@/hooks/useClustering';
+import { MarkerCondition } from '@/store/cards';
+import { useFavoritesStore } from '@/store/favorites';
+import React, { useCallback, useEffect, useState } from 'react'
+import { Marker, Popup } from 'react-leaflet';
+import ListOfCardsPopup from './list-of-cards-popup';
+import CardPopup from './cart-popup';
+
+type Props = {
+    cluster: Cluster,
+    clusters: Cluster[],
+    updateCondition: (id: number, condition: MarkerCondition) => void
+}
+
+export const CustomMarker = ({cluster, clusters, updateCondition}: Props) =>{
+    const [L, setL] = useState<any>(null);
+
+    useEffect(() => {
+        import("leaflet").then((leaflet) => setL(leaflet));
+      }, []);
+    
+    const favorites = useFavoritesStore();
+
+    const singleMarker = (condition:MarkerCondition, price:number, id: number) => {
+    const fav = favorites.inFavList(id);
+        return `<div class="marker-container ${condition} flex gap-1 hover:scale-[106%] transition-all duration-300">
+                    <span class="marker-text">${price}₽</span>
+                    ${fav ? `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="${condition === MarkerCondition.ACTIVE?"white":"red"}">
+                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+                    </svg>` : ""}
+                </div>`;
+    };
+    
+    
+    const clusterMarker = (condition: MarkerCondition) => {
+    return  ` <div class="${condition} cluster-marker  flex  hover:scale-[106%] transition-all duration-300">
+            </div>`
+    ;
+    }
+    
+
+    const createCustomIcon = useCallback(
+    (cluster:Cluster) => {
+        if (!L) return null;
+        return L.divIcon({
+        className: "custom-marker",
+        html: cluster.cardItems.length > 1 ? clusterMarker(cluster.condition) : singleMarker(cluster.condition, cluster.cardItems[0].price, cluster.id),
+        iconSize: [50, 30],
+        iconAnchor: [25, 15],
+        });
+    },
+    [L, favorites]
+    );
+
+    const icon = createCustomIcon(cluster);
+    
+    return (
+      icon && (
+        <Marker
+          key={cluster.cardItems[0].id}
+          icon={icon}
+          position={[cluster.cardItems[0].coordinates.lat, cluster.cardItems[0].coordinates.lng]}
+          eventHandlers={{
+            click: () => {
+              clusters.map(cl => {
+                if (cl.condition === MarkerCondition.ACTIVE) {
+                  updateCondition(cl.id, MarkerCondition.VISITED);
+                }
+                return cl;
+              });
+              updateCondition(cluster.id, MarkerCondition.ACTIVE);
+            },
+          }}
+        >
+          <Popup>
+            {cluster.cardItems.length > 1 ? (
+              <ListOfCardsPopup cardItems={cluster.cardItems}/>
+            ) : (
+            <CardPopup inFavList={favorites.inFavList} clickToFav={favorites.clickToFav} cardItem={cluster.cardItems[0]} />
+            )}
+          </Popup>
+        </Marker>
+      )
+    );
+    
+}
+
